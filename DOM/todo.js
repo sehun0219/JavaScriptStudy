55335cf842b9fab51c172e1a57e668bedf619ef5
sehun <cloudnine0219@gmail.com>
2023-04-13 12:12:42 +1200

    각 함수기능 주석으로 설명추가

--- DOM/todo.js
+++ DOM/todo.js
@@ -1,24 +1,31 @@
+// HTML 요소를 DOM 객체로 가져옴
 const inputDom = document.getElementById("todo-input");
 const buttonDom = document.getElementById("todo-button");
 const todoListDom = document.getElementById("todo-list");
+// 할 일 텍스트 및 할 일 목록 초기화
 let todoText = "";
 const todoList = JSON.parse(window.sessionStorage.getItem("todoList")) || [];
+
+// 다음 할 일의 ID 값을 설정
 //[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }];
-let todoId = todoList[todoList.length - 1] // 마지막인덱스 표시
+let todoId = todoList[todoList.length - 1]
   ? todoList[todoList.length - 1].id + 1
   : 0;
 
+// 할 일 목록을 세션 저장소에 저장하는 함수
 const saveTodoList = () => {
   window.sessionStorage.setItem("todoList", JSON.stringify(todoList));
 };
-
+// 할 일 텍스트 입력 필드 초기화 함수
 const resetTextInput = () => {
   todoText = "";
   inputDom.value = "";
 };
 
+// 할 일 목록을 화면에 표시하는 함수
 const renderTodoList = () => {
   todoListDom.innerHTML = "";
+  // 할 일 목록을 담고 있는 요소의 내용을 비우는 코드. 이렇게 하면 이전 목록이 지워지고 새로운 목록이 추가될 때 깔끔하게 보임.
   // todoList.forEach((todo) => {
   //   const todoItemDom = createdTodoItemDom(todo);
   //   todoListDom.appendChild(todoItemDom);
@@ -28,13 +35,12 @@ const renderTodoList = () => {
     todoListDom.appendChild(todoItemDom);
   }
 };
-
+// 각 할 일을 나타내는 DOM 요소를 생성하는 함수 (완료 처리 및 삭제 기능 포함)
 const createdTodoItemDom = ({ isDone, text, id }) => {
   const todoWrapDom = document.createElement("div");
   const todoTextDom = document.createElement("span");
   const todoCheckDom = document.createElement("input");
   const todoDeleteButton = document.createElement("button");
-
   todoDeleteButton.innerText = "삭제";
   todoCheckDom.type = "checkbox";
   todoCheckDom.checked = isDone;
@@ -63,7 +69,7 @@ const createdTodoItemDom = ({ isDone, text, id }) => {
   todoWrapDom.appendChild(todoTextDom);
   return todoWrapDom;
 };
-
+// 할 일을 추가하는 함수
 const addTodo = () => {
   const newTodoObj = { id: todoId++, text: todoText, isDone: false };
   todoList.push(newTodoObj);
@@ -72,20 +78,14 @@ const addTodo = () => {
   saveTodoList();
 };
 
+// 입력된 할 일 텍스트를 변경하는 함수
 const changeTodoText = (event) => {
   todoText = event.target.value;
 };
 
+// 이벤트 리스너 설정
 inputDom.addEventListener("input", changeTodoText);
-
 buttonDom.addEventListener("click", addTodo);
 
-// 할일을 완료처리 / done 처리
-
-// 할 일 삭제 기능: 할 일을 목록에서 삭제할 수 있는 버튼.
-
-// 필터 기능: 전체 할 일, 완료된 할 일, 미완료 할 일 등 다양한 카테고리로 목록을 필터링 할 수 있는 기능.
-
-// 저장 및 로드 기능: 할 일 목록을 로컬 스토리지에 저장하고, 앱을 다시 실행할 때 로드 할 수 있는 기능.
-
+// 초기 할 일 목록 표시
 renderTodoList();
